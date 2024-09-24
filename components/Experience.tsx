6752ee6b5e2494b4b6c72335d38d3b2f4f3409e2
Suvadip Mahato <mahatosuvadip20@gmail.com>
2024-09-24 20:49:46 +0530

    fix

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { workExperience } from "@/data";
 import { Button } from "./ui/MovingBorders";
 
-const Experience = () => {};
+const Experience = () => {
+    return <></>
+};
 
 export default Experience;
