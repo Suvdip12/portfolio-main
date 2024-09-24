7c8a6a53a276ed6dfe42e9d4d09a209e28a95595
Suvadip Mahato <mahatosuvadip20@gmail.com>
2024-09-24 20:33:31 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,175 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "I prioritize client collaboration,fostering transparent and strategic communication",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I’m very flexible and adaptable to different time zones",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "copywriting",
+    description: "I constantly honing my strive for",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Copywriting enthusiast with a passion for persuasive storytelling.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title:
+      "Currently refining a storytelling framework for more impactful brand messaging",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Ready to collaborate and bring your brand’s voice to life?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "",
+    des: "Instagram caption,bio and description mastery foraudience retention.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "",
+    des: "Product description,tagline and brand stories",
+    img: "/dauntless copy.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "",
+    des: "Portfolio and landing pages like the one you are seeing.",
+    img: "/Screenshot.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "",
+    des: "Customized work according to your need",
+    img: "/title_page_image.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Mahi was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Mahi's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Mahi is the ideal partner.",
+    name: "Surya Prakash Tiwary",
+    title: "content creator and podcastor",
+  },
+  {
+    quote:
+      "Collaborating with Mahi was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Mahi's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Mahi is the ideal partner.",
+    name: "Surya Prakash Tiwary",
+    title: "content creator and podcastor",
+  },
+  {
+    quote:
+      "Collaborating with Mahi was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Mahi's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Mahi is the ideal partner.",
+    name: "Surya Prakash Tiwary",
+    title: "content creator and podcastor",
+  },
+  {
+    quote:
+      "Collaborating with Mahi was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Mahi's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Mahi is the ideal partner.",
+    name: "Surya Prakash Tiwary",
+    title: "content creator and podcastor",
+  },
+  {
+    quote:
+      "Collaborating with Mahi was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Mahi's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Mahi is the ideal partner.",
+    name: "Surya Prakash Tiwary",
+    title: "content creator and podcastor",
+  },
+];
+
+export const companies = [];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [];
